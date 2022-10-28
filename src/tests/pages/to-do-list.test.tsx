d9bd78a509e015944906ea62ce2abce61719e12d
anapgonza <anapgonza@ext.zara.com>
2022-10-28 19:52:31 +0200

    fix: to-do-list tests

--- src/tests/pages/to-do-list.test.tsx
+++ src/tests/pages/to-do-list.test.tsx
@@ -1,21 +1,21 @@
 import { render, screen } from '@testing-library/react';
-import App from '../../App';
+import ToDoList from '../../pages/to-do-list/to-do-list';
 
 describe('<ToDoList /> test suite', () => {
 	test('should render to-do-list container', () => {
-		render(<App />);
+		render(<ToDoList />);
 		const listContainer = screen.getByTestId('to-do-list');
 		expect(listContainer).toBeInTheDocument();
 	});
 
 	test('should render a form', () => {
-		render(<App />);
+		render(<ToDoList />);
 		const form = screen.getByTestId('task-form');
 		expect(form).toBeInTheDocument();
 	});
 
 	test('should render a list', () => {
-		render(<App />);
+		render(<ToDoList />);
 		const list = screen.getByTestId('task-list');
 		expect(list).toBeInTheDocument();
 	});
